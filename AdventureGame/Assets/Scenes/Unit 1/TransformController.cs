5b6325c563c46fb1b7e1fc0251e974d93d93f1b7
dripula <dvlanmartin@gmail.com>
2024-09-13 13:53:11 -0600

    manipulated a cube using transformcontroller

--- AdventureGame/Assets/Scenes/Unit 1/TransformController.cs
+++ AdventureGame/Assets/Scenes/Unit 1/TransformController.cs
@@ -6,10 +6,10 @@ public class TransformController : MonoBehaviour
 
     void Update()
     {
-        // Move the target GameObject up and down
-       
-        // Rotate the target GameObject
-       
-        // Scale the target GameObject
+
+        var x = Mathf.PingPong(Time.time, 3); transform.position = new Vector3(x,0,0);
+        
+        transform.Rotate(new Vector3(0,60,0) * Time.deltaTime);
+
     }
 }
